31d4140484781d2d95cec60ad8569081084ce995
hb <2606526753@qq.com>
2020-05-29 22:45:34 +0800

    何斌：分支4

--- main.java
+++ main.java
@@ -7,12 +7,14 @@ public class main {
     }
     public static void flower() {
         int x = 0,y = 0,z = 0;
+        int hb;
         for(int i = 100;i<1000;i++) {
             x = i/100;
             y = (i%100)/10;
             z = (i%100)%10;
             if(x*x*x + y*y*y +z*z*z == i) {
                 System.out.print(i+" ");
+
             }
         }
     }
